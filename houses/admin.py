9d6f225bd1d682cba022ad2f87ddd8ed763360bb
오나성 <ohnaseong@ohnaseong-MacBookPro.local>
2022-11-11 14:45:36 +0900

    2.1 Custom Model

--- houses/admin.py
+++ houses/admin.py
@@ -6,13 +6,6 @@ from houses.models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    fields = (
-        "name",
-        "address",
-        ("price_per_night", "pets_allowed"),
-    )
+    
     list_display = ("name", "price_per_night", "address", "pets_allowed")
     list_filter = ("price_per_night", "pets_allowed")
-    search_fields = ("address",)
-    list_display_links = ("name", "address",)
-    list_editable = ("pets_allowed",)
