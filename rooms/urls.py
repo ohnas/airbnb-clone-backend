dfe76544cd8813f74d42614de8cec1e347b2087e
ohnas <ohnas12@gmail.com>
2022-11-13 19:56:09 +0900

    6.6 Django vs React

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from rooms.views import see_one_room, see_all_room
 
-urlpatterns = [
-    path("", see_all_room),
-    path("<int:room_pk>/", see_one_room),
-]
+
+urlpatterns = []
