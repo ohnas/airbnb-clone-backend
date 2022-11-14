d039afa68e60a849c04fd0e5fd8593e88d0e7779
ohnas <ohnas12@gmail.com>
2022-11-14 14:26:06 +0900

    8.0 All Amenities

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rooms.views import Amenities, AmenityDetail
 
 
-urlpatterns = []
+urlpatterns = [
+    path("amenities/", Amenities.as_view()),
+    path("amenities/<int:pk>/", AmenityDetail.as_view()),
+]
