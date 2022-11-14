a9fbf1947669e0a66a6742cef41d6aa08ab88460
ohnas <ohnas12@gmail.com>
2022-11-14 11:06:18 +0900

    7.4 POST Requests

--- categories/urls.py
+++ categories/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from categories.views import categories
+from categories.views import categories, category
 
 urlpatterns = [
     path("", categories),
+    path("<int:pk>/", category),
 ]
