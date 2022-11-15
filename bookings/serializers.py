f6081d84ad9cae75f6cb999d3513f7bbc59b5e68
ohnas <ohnas12@gmail.com>
2022-11-15 17:09:49 +0900

    8.23 Create a Booking

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,5 +1,33 @@
+from django.utils import timezone
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from bookings.models import Booking
+
+
+class CreateRoomBookingSerializer(ModelSerializer):
+
+    check_in = serializers.DateField()
+    check_out = serializers.DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Can't book in the past!")
+        return value
+
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Can't book in the past!")
+        return value
 
 
 class PublicBookingSerializer(ModelSerializer):
