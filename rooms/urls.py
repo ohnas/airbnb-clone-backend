43e4b35c75c69f80820e5aa6641fe93116246ca7
ohnas <ohnas12@gmail.com>
2022-11-15 13:32:08 +0900

    8.15 File Uploads

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,6 +6,7 @@ from rooms.views import (
     RoomDetail,
     RoomReviews,
     RoomAmenities,
+    RoomPhotos,
 )
 
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path("<int:pk>/", RoomDetail.as_view()),
     path("<int:pk>/reviews", RoomReviews.as_view()),
     path("<int:pk>/amenities", RoomAmenities.as_view()),
+    path("<int:pk>/photos", RoomPhotos.as_view()),
     path("amenities/", Amenities.as_view()),
     path("amenities/<int:pk>/", AmenityDetail.as_view()),
 ]
