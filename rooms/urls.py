ba6cb38dc422409d9d343a809376b43f937fe768
ohnas <ohnas12@gmail.com>
2022-11-15 12:58:20 +0900

    8.14 Paginagion

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from rooms.views import Amenities, AmenityDetail, Rooms, RoomDetail
+from rooms.views import (
+    Amenities,
+    AmenityDetail,
+    Rooms,
+    RoomDetail,
+    RoomReviews,
+    RoomAmenities,
+)
 
 
 urlpatterns = [
     path("", Rooms.as_view()),
     path("<int:pk>/", RoomDetail.as_view()),
+    path("<int:pk>/reviews", RoomReviews.as_view()),
+    path("<int:pk>/amenities", RoomAmenities.as_view()),
     path("amenities/", Amenities.as_view()),
     path("amenities/<int:pk>/", AmenityDetail.as_view()),
 ]
