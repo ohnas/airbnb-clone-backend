5f9bf94a14e99c521dbce44bc9a53b730adb3cc0
ohnas <ohnas12@gmail.com>
2022-11-16 12:15:55 +0900

    9.1 Create User

--- users/views.py
+++ users/views.py
@@ -2,7 +2,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError, NotFound
 from users.serializers import PrivateUserSerializer
+from users.models import User
 
 
 class Me(APIView):
@@ -23,6 +25,22 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
