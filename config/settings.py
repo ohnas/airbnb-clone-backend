587acc1fbba48b3fc4932896173ea9952baa8e7f
ohnas <ohnas12@gmail.com>
2022-11-21 13:58:56 +0900

    13.2 CORS_ALLOW_CREDENTIALS

--- config/settings.py
+++ config/settings.py
@@ -156,4 +156,8 @@ MEDIA_URL = "user-uploads/"
 
 PAGE_SIZE = 3
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
